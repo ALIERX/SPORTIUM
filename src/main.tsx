02edb64e31f29c2e93d4982ddd514b38e5521380
Alex Khamraev <a.khamraev@gmail.com>
2025-10-15 12:52:13 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,12 @@
-
+import { AuthProvider } from './lib/AuthContext';
+// ...
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
